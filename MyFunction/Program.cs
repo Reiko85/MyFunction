74c21e0c06e9a616d5b911a5d06aa35fff04c1a4
Opilane TTHK <opilane@tthk.ee>
2019-10-17 17:04:58 +0300

    first commit

---
+++ MyFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string color;
+            Console.WriteLine("Name");
+            name = Console.ReadLine();
+            Console.WriteLine("color");
+            color = Console.ReadLine();
+            KnowMe(name, color);
+
+            
+                
+        }
+
+
+
+        public static void KnowMe(string name, string color)
+        {
+            Console.WriteLine($"Hello {name}, Your favourite color is {color}");
+
+        }
+    }
+}
